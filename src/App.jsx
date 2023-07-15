0ad004cfb51b71285a728745115fc9ca19637346
Md. Kawsar Ahammed <mdkawsarahammed8875@gmail.com>
2023-07-15 11:14:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header title="React Application" />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
